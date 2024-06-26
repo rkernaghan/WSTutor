f2aaf1a186387076c20b4a0751f9586397c2a526
Russell Kernaghan <rskernaghan@gmail.com>
2024-06-26 12:48:17 -0500

    Initial Commit

---
+++ WSTutor/WSTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WSTutorApp.swift
+//  WSTutor
+//
+//  Created by Russell Kernaghan on 2024-06-26.
+//
+
+import SwiftUI
+
+@main
+struct WSTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
